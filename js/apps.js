deee271b844889fd7f2b994a14bdd0ebc91d8a2e
Bateyjosue <josuebatey19@gmail.com>
2022-07-27 15:57:41 +0200

    Milestone 4: Interaction About link, menu on mobile

--- js/apps.js
+++ js/apps.js
@@ -4,4 +4,20 @@ const CloseMenu = document.querySelector('.close-menu');
 
 menuHumberge.addEventListener('click', () => {
   nav.style.visibility = 'visible';
+  nav.style.display = 'block';
 });
+
+CloseMenu.addEventListener('click', () => {
+  nav.style.visibility = 'hidden';
+  nav.style.display = 'none';
+});
+
+const guestInfo = [
+  {
+    id: 'guest1',
+    nam: 'Roga Roga',
+    country: 'Congo-Brazza',
+    description: 'Real name Ibambi Okombi Rogatien is a 48-year-old Congolese singer.',
+    img: './images.roga,jpg'
+  }
+]
