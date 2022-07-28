cdc3d2925dbd0afa48075ea0703261ed9000eb36
Bateyjosue <josuebatey19@gmail.com>
2022-07-28 13:04:20 +0200

    Fix menu top where the nav is shown up

--- js/apps.js
+++ js/apps.js
@@ -1,15 +1,23 @@
 const menuHumberge = document.querySelector('.menu-humberger');
 const nav = document.querySelector('nav');
 const CloseMenu = document.querySelector('.close-menu');
-const blurryBG = document.querySelector('section, footer') 
+const past = document.querySelectorAll('.past .first');
+const body = document.querySelector('body');
 
 menuHumberge.addEventListener('click', () => {
   nav.style.display = 'block';
+  past.forEach((element) => {
+    element.style.visibility = 'hidden';
+  });
+  body.style.overflow = 'hidden';
 });
 
 CloseMenu.addEventListener('click', () => {
-  nav.style.visibility = 'hidden';
   nav.style.display = 'none';
+  past.forEach((element) => {
+    element.style.visibility = 'visible';
+    body.style.overflow = 'none';
+  });
 });
 
 const guestInfo = [
@@ -18,54 +26,49 @@ const guestInfo = [
     nam: 'Roga Roga',
     country: 'Congo-Brazza',
     description: 'Real name Ibambi Okombi Rogatien is a 48-year-old Congolese singer.',
-    img: './images/roga.jpg'
-  }
-  ,
+    img: './images/roga.jpg',
+  },
   {
     id: 'guest2',
     nam: 'Robinio',
     country: 'DRC',
     description: 'Mongala Akelembi aka Robinio Mundibu is a Congolese artist.',
-    img: './images/robi.jpg'
-  }
-  ,
+    img: './images/robi.jpg',
+  },
   {
     id: 'guest3',
     nam: 'Charly na Nina',
     country: 'Rwanda',
     description: 'Girl power for this Rwandan music group made up of 2 women',
-    img: './images/nina.jpg'
-  }
-  ,
+    img: './images/nina.jpg',
+  },
   {
     id: 'guest4',
     nam: 'Mohombi',
     country: 'Drc-Sweden',
     description: 'is a singer, dancer, producer, composer and author of pop and African music, born to a Congolese father and a Swedish mother.',
-    img: './images/mohombi.jpeg'
-  }
-  ,
+    img: './images/mohombi.jpeg',
+  },
   {
     id: 'guest5',
     nam: 'W Malick',
     country: 'Drc-North-Kivu',
     description: 'is a Congolese artist from Butembo, where he started music with other friends.',
-    img: './images/malick.jpg'
-  }
-  ,
+    img: './images/malick.jpg',
+  },
   {
     id: 'guest6',
     nam: 'Alesh',
     country: 'Drc-Kinshasa',
     description: 'whose real name is Alain Chirwisa, is a Congolese musician, committed rapper and songwriter',
-    img: './images/alesh.jpeg'
-  }
-]
+    img: './images/alesh.jpeg',
+  },
+];
 
 const showMore = document.querySelector('#show-more');
 const card = document.querySelector('.card');
 
-guestInfo.forEach((element, index) => {
+guestInfo.forEach((element) => {
   card.innerHTML += `
     <li class="">
       <div class="img-card">
@@ -88,15 +91,14 @@ guestInfo.forEach((element, index) => {
 const cards = document.querySelectorAll('.card li');
 
 showMore.addEventListener('click', () => {
-  if(showMore.innerText === 'LESS'){
+  if (showMore.innerText === 'LESS') {
     showMore.innerText = 'MORE';
     cards.forEach((element, index) => {
       if (index >= 2) {
         element.classList.add('hide-card');
       }
     });
-  }
-  else {
+  } else {
     showMore.innerText = 'LESS';
     cards.forEach((element, index) => {
       if (index >= 2) {
